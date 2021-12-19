6917a015e5f686efc6c76d94c83195d9bde519f4
Vincent Sureau <vincentsureau5@gmail.com>
2021-12-19 15:29:34 +0100

    hotfix: affichage conditionnel pour j = 1 et i = 1

--- Script3.js
+++ Script3.js
@@ -7,7 +7,6 @@ for(var i = 1; i <= 10; i++) {
 	document.write("<tr style='height:30px;'>");
 
 	for(var j = 1; j <= 10; j++) {
-        
 		
         if(j> 1 && i == 1 || i>1 && j==1) {
 			color = "grey";
@@ -27,7 +26,7 @@ for(var i = 1; i <= 10; i++) {
 		}
         
 		// document.write("<td style='width:30px;background-color:" + color+ ";text-align:center;font-weight:"+fontweight+"'>" + i*j + "</td>");
-        document.write(`<td style='width:30px;background-color:${color};text-align:center;font-weight:${fontweight}'>${(i*j == 1) ? `${x}` : `${i*j}`}</td>`);
+        document.write(`<td style='width:30px;background-color:${color};text-align:center;font-weight:${fontweight}'>${(i*j == 1) ? `x` : i*j}</td>`);
 
     }
 	document.write("</tr>");
